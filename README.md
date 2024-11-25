990e543a90d26bb94710fc8d1d211ea6e7fdca83
Thu Yein Tun <yon.fu111@gmail.com>
2024-11-25 10:15:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Guided Practice - Party Planner
+
+This guided practice uses the same party API as the previous workshop. A list of all parties is shown. When a user clicks on a specific party, the page is updated to show the details of that party. The selected party persists even if the user refreshes the page.
+
+Your main task is to show a list of all the guests for the selected party.
+
+Read [the API documentation](https://fsa-crud-2aa9294fe819.herokuapp.com/api/). You will be working with the `/events`, and `/guests` endpoints.
+
+## Prompts
+
+The answers can be viewed directly below the respective prompt. The `solution` branch contains the final code.
+
+1. Write `getGuests()`. The goal is to make a GET request to the `guests` endpoint and update state with the response data. You can check `state` in the console to see if the data is successfully fetched.
+
+   <details>
+   <summary>Show Answer</summary>
+
+   ```js
+   async function getGuests() {
+     try {
+       const response = await fetch(API + "/guests");
+       const json = await response.json();
+       state.guests = json.data;
+     } catch (error) {
+       console.error(error);
+     }
+   }
+   ```
+
+   </details>
+
+2. Refactor `renderGuests()` to render the list of guests for the selected party. The guest list should show the name, email, and phone number of each attending guest. This will require you to look through the RSVPs for the selected party and find the guests that are attending. Use array methods like `filter`, `map`, and `includes`!
+
+   <details>
+   <summary>Show Answer</summary>
+
+   ```js
+   function renderGuests() {
+     $guests.hidden = false;
+
+     // Get guests for the current party
+     const rsvps = state.rsvps.filter(
+       (rsvp) => rsvp.eventId === state.event.id
+     );
+     const guestIds = rsvps.map((rsvp) => rsvp.guestId);
+     const guests = state.guests.filter((guest) => guestIds.includes(guest.id));
+
+     if (!guests.length) {
+       $guestList.innerHTML = "<li>No guests yet!</li>";
+       return;
+     }
+
+     const guestList = guests.map((guest) => {
+       const guestInfo = document.createElement("li");
+       guestInfo.innerHTML = `
+         <span>${guest.name}</span>
+         <span>${guest.email}</span>
+         <span>${guest.phone}</span>
+       `;
+       return guestInfo;
+     });
+
+     $guestList.replaceChildren(...guestList);
+   }
+   ```
+
+   </details>
---
+++ index.js
@@ -0,0 +1,140 @@
+const COHORT = "REPLACE_ME!";
+const API = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/" + COHORT;
+
+const state = {
+  events: [],
+  event: null,
+  guests: [],
+  rsvps: [],
+};
+
+// The $ prefix is used here to denote variables that reference DOM elements
+const $eventList = document.querySelector("#eventList");
+const $eventDetails = document.querySelector("#eventDetails");
+const $guests = document.querySelector("#guests");
+const $guestList = document.querySelector("#guestList");
+
+window.addEventListener("hashchange", selectEvent);
+
+/**
+ * Update state with data from the API and the DOM to reflect current state
+ */
+async function render() {
+  await getEvents();
+  await getGuests();
+  await getRsvps();
+
+  renderEvents();
+  selectEvent();
+}
+
+render();
+
+/**
+ * Show details about the currently selected event
+ */
+function selectEvent() {
+  getEventFromHash();
+  renderEventDetails();
+}
+
+/**
+ * Find the event that matches the current hash to update state
+ */
+function getEventFromHash() {
+  // We need to slice the # off
+  const id = window.location.hash.slice(1);
+  state.event = state.events.find((event) => event.id === +id);
+}
+
+/**
+ * GET the list of guests from the API to update state
+ */
+async function getGuests() {
+  // TODO
+}
+
+/**
+ * Render the list of guests for the currently selected event
+ */
+function renderGuests() {
+  $guests.hidden = false;
+
+  // TODO: Render the list of guests for the currently selected event
+  $guestList.innerHTML = "<li>No guests yet!</li>";
+}
+
+// === No need to edit anything below this line! ===
+
+/**
+ * GET the list of events from the API to update state
+ */
+async function getEvents() {
+  try {
+    const response = await fetch(API + "/events");
+    const json = await response.json();
+    state.events = json.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * GET the list of rsvps from the API to update state
+ */
+async function getRsvps() {
+  try {
+    const response = await fetch(API + "/rsvps");
+    const json = await response.json();
+    state.rsvps = json.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * Update `$eventList` to reflect the current state
+ */
+function renderEvents() {
+  const events = state.events.map(renderEvent);
+  $eventList.replaceChildren(...events);
+}
+
+/**
+ * @param {Event} event the event to render
+ * @returns {HTMLElement} an article element representing the event
+ */
+function renderEvent(event) {
+  const article = document.createElement("article");
+  const date = event.date.slice(0, 10);
+
+  article.innerHTML = `
+    <h3><a href="#${event.id}">${event.name} #${event.id}</a></h3>
+    <time datetime="${date}">${date}</time>
+    <address>${event.location}</address>
+  `;
+
+  return article;
+}
+
+/**
+ * Render details about the currently selected event
+ */
+function renderEventDetails() {
+  if (!state.event) {
+    $eventDetails.innerHTML = "<p>Select a event to see more.</p>";
+    $guests.hidden = true;
+    return;
+  }
+
+  const date = state.event.date.slice(0, 10);
+
+  $eventDetails.innerHTML = `
+    <h2>${state.event.name} #${state.event.id}</h2>
+    <time datetime="${date}">${date}</time>
+    <address>${state.event.location}</address>
+    <p>${state.event.description}</p>
+  `;
+
+  renderGuests();
+}
